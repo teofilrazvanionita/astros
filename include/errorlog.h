1535176ab5cf9f03354ced669110bcbc0709a5e2
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2019-01-21 03:27:56 +0200

    reconfigured macros: transformed into inline functions

--- include/errorlog.h
+++ include/errorlog.h
@@ -1,7 +1,6 @@
 #ifndef __ERRORLOG_H
 #define __ERRORLOG_H
 
-#include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <errno.h>
@@ -9,8 +8,8 @@
 
 #include "time_and_date.h"
 
-#define ERROR(msg)	char temp[80]; memset(temp, 0, 64); sprintf(temp, "%s [%s]:%d " msg ": " "errno %d: %s\n", getTime(), __FILE__, __LINE__, errno, strerror(errno)); write(STDERR_FILENO, temp, strlen(temp));
-#define PTHREAD_ERROR(msg, err)	char temp[80]; memset(temp, 0, 64); sprintf(temp, "%s [%s]:%d " msg ": " "errnum %d: %s\n", getTime(), __FILE__, __LINE__, err, strerror(err)); write(STDERR_FILENO, temp, strlen(temp));
+void ERROR (const char *msg);
+void PTHREAD_ERROR (const char *msg, int err);
 
 #ifdef DEBUG
 #define Dprintf(msg,...) printf("%s [%s]:%d " msg, getTime(), __FILE__, __LINE__, ##__VA_ARGS__)
--- source/time_and_date.c
+++ source/time_and_date.c
@@ -1,10 +1,13 @@
 #include "time_and_date.h"
 
-char * getTime(void)
+char *time_and_date;
+
+char * getTime (void)
 {	
 	time_t t;
 	char *p;
-	char *time_and_date = malloc(27*sizeof(char));
+	
+	time_and_date = malloc(27*sizeof(char));
 
 	t = time(NULL);
 	ctime_r((const time_t *)&t, time_and_date);
@@ -13,4 +16,9 @@ char * getTime(void)
 	*p = 0;
 
 	return time_and_date;
+}
+
+void destructTime (void)
+{
+	free(time_and_date);
 }
