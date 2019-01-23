ae028cf1f582e28584ee7f0d0d8b835ed3ff4ab3
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2019-01-23 17:41:26 +0200

    Implemented mutex

--- include/connection_threads.h
+++ include/connection_threads.h
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #include "errorlog.h"
 
--- source/connection_threads.c
+++ source/connection_threads.c
@@ -1,15 +1,22 @@
 #include "connection_threads.h"
 
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 CONNECTION *first, *last;
 
 void addConnection(int remote_sfd, struct sockaddr their_addr)
 {
 	CONNECTION *p; 
-	int i;
+	int i, s;
 
 	if((p = (CONNECTION *)malloc(sizeof(CONNECTION))) == 0){
 		ERROR("insufficient memory at list creation");
 		exit(EXIT_FAILURE);	
+	}
+
+	s = pthread_mutex_lock(&mtx);
+	if(s != 0){
+		PTHREAD_ERROR("pthread_mutex_lock", s);
+		exit(EXIT_FAILURE);
 	}
 
 	if((i=loadNOD(p, remote_sfd, their_addr))!=1){
@@ -24,6 +31,12 @@ void addConnection(int remote_sfd, struct sockaddr their_addr)
 
 	last = p;
 	last -> next = NULL;
+	
+	s = pthread_mutex_unlock(&mtx);
+	if(s != 0){
+		PTHREAD_ERROR("pthread_mutex_lock", s);
+		exit(EXIT_FAILURE);
+	}
 }
 
 int loadNOD(CONNECTION *p, int remote_sfd, struct sockaddr their_addr)
@@ -31,7 +44,7 @@ int loadNOD(CONNECTION *p, int remote_sfd, struct sockaddr their_addr)
 	int s;
 
 	p -> state = TS_ALIVE;
-	s = pthread_create(&(p->tid), NULL, threadFunc, NULL);
+	s = pthread_create(&(p->tid), NULL, threadFunc, p);
 	if (s != 0){
 		PTHREAD_ERROR("pthread_create", s);
 		return 0;
@@ -49,6 +62,15 @@ void freeNOD(CONNECTION *p)
 
 static void * threadFunc(void *arg)
 {
+
+	CONNECTION *p = (CONNECTION *) arg;
+
+
+	if(close(p -> remote_sfd) == -1){
+		ERROR("close");
+		exit(EXIT_FAILURE);
+	}
+	p -> state = TS_TERMINATED;
 
 	return NULL;
 }
