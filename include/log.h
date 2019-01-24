79b9ad3334d5e1b3569aaf12558a430c5f1d5d8c
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2019-01-24 19:38:25 +0200

    developed log connection procedure

--- include/log.h
+++ include/log.h
@@ -1,7 +1,20 @@
 #ifndef __LOG_H
 #define __LOG_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#include "errorlog.h"
+
 #define LOG_FILE "/var/log/maillog"
 
+void logConnectFrom (struct sockaddr *their_addr_p);
 
 #endif
---
+++ source/log.c
@@ -0,0 +1,40 @@
+#include "log.h"
+
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+void logConnectFrom (struct sockaddr *their_addr_p)
+{
+	int log_fd, s;
+	char ip_addr[16];
+	char connection_from[35];
+
+	memset(ip_addr, 0, 16);
+	memset(connection_from, 0, 35);
+
+	if((log_fd = open(LOG_FILE, O_WRONLY | O_APPEND)) == -1){
+		ERROR("open");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(ip_addr, inet_ntoa((((struct sockaddr_in *)their_addr_p)->sin_addr)));
+	sprintf(connection_from, "Connection from: %s\n", ip_addr);
+	
+	s = pthread_mutex_lock(&mtx);
+	if (s != 0){
+		PTHREAD_ERROR("pthread_mutex_lock", s);
+		exit(EXIT_FAILURE);
+	}
+	if(write(log_fd, connection_from, sizeof(connection_from)) == -1){
+		ERROR("write");
+		exit(EXIT_FAILURE);
+	}
+	s = pthread_mutex_unlock(&mtx);
+	if (s != 0){
+		PTHREAD_ERROR("pthread_mutex_lock", s);
+		exit(EXIT_FAILURE);
+	}
+
+	if(close(log_fd) == -1){
+		ERROR("close");
+		exit(EXIT_FAILURE);
+	}
+}
