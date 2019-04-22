09ec9e5783e08caa2c4c4a5c6cf92e65bc1d2fc9
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2019-04-22 18:58:25 +0300

    message.h further developed

--- include/message.h
+++ include/message.h
@@ -1,12 +1,23 @@
 #ifndef __MESSAGE_H
 #define __MESSAGE_H
 
+#define DATA_CHUNK_SIZE 80
+
+typedef struct RCPT_TO {
+
+	struct RCPT_TO *next;
+} RCPT_TO;
+
 typedef struct DATA {
 
+	struct DATA *next;
 } DATA;
 
 typedef struct MESSAGE {
+	char *from;
+	RCPT_TO *rcpt_to;
 
+	DATA *data;
 } MESSAGE;
 
 #endif
