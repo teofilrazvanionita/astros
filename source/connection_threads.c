9c228f30b74a52f51fa7c58a70aba91e5a7281d3
Teofil Razvan Ionita <teofilrazvanionita@gmail.com>
2019-12-12 13:24:22 +0200

    Added TODO to code

--- source/connection_threads.c
+++ source/connection_threads.c
@@ -62,7 +62,7 @@ int loadNOD (CONNECTION *p, int remote_sfd, struct sockaddr their_addr)
 
 void freeNOD (CONNECTION *p)
 {
-	Dprintf("freeing memory\n"); // ToDo: Dprintf more info about the freed node
+	Dprintf("freeing memory\n"); // TODO: Dprintf more info about the freed node
 	free(p);
 }
 
