b1a8a9fbd8aa25053a38729fde139a970137fcfc
teofilrazvanionita@gmail.com <>
2019-12-12 13:32:54 +0200

    Updated TODO: tag

--- source/connection_threads.c
+++ source/connection_threads.c
@@ -62,7 +62,7 @@ int loadNOD (CONNECTION *p, int remote_sfd, struct sockaddr their_addr)
 
 void freeNOD (CONNECTION *p)
 {
-	Dprintf("freeing memory\n"); // TODO: Dprintf more info about the freed node
+	Dprintf("freeing memory\n"); // TODO: Dprintf more info about the freed node, like time and date maybe...
 	free(p);
 }
 
